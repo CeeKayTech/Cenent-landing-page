0b46a259b1feb9e62ab69c806fc0ba4756cf3929
CeeKay <94992531+CeeKayTech@users.noreply.github.com>
2022-04-17 12:24:40 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'red-bg': '#d76a66',
+        'blue-bg': '#0000fe',
+        'blue': '#273067',
+        'purple': '#230846',
+        'purple2': '#38086c',
+        
+      },
+    },
+  },
+  plugins: [],
+}
